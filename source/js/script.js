59b5b6aa6749636dbd44bc982b0a501a732b0258
Dmitry Dolgushin <razor9714@gmail.com>
2020-04-23 10:22:39 +0500

    добавил плавную прокрутку до якоря

--- source/js/script.js
+++ source/js/script.js
@@ -63,3 +63,18 @@ form.addEventListener('submit', function () {
     localStorage.setItem('question', question.value);
   }
 });
+
+var anchors = document.querySelectorAll('a[href*="#"]')
+
+anchors.forEach(function (anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    var blockID = anchor.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
